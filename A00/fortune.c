263d7d6af04b25fb86fd9c87efe64762cdc88f2d
tfeshbach <tfeshbach@brynmawr.edu>
2022-08-30 15:13:17 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Talia Feshbach
+ * Date: 8/30/2022
+ * Print a fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Be receptive to new opportunities\n");
+  return 0;
+}
