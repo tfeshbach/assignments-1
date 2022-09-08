4c35b698d6e99805f9739595e071c7f58b23606a
alinen <alinen@savvysine.com>
2022-09-08 11:55:23 -0400

    A01 basecode

--- A01/wordle.c
+++ A01/wordle.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "words.h" // header that defined chooseWord()
+
 int main() {
+  srand(time(0));
+  const char* word = chooseWord();
+  printf("cheat: %s\n", word);
+
   return 0;
 }
