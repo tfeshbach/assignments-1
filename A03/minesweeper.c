009ad6f260e7a5810ff3fde03dc69266b296bd20
Talia Feshbach <tfeshbach@goldengate.cs.brynmawr.edu>
2022-09-30 22:47:36 -0400

    A03 complete

--- A03/minesweeper.c
+++ A03/minesweeper.c
@@ -1,3 +1,123 @@
-int main() {
+//Author: Talia Feshbach
+//Date: 9/30/22
+//Name: Minesweeper
+
+#include <stdio.h>
+#include <stdlib.h> //includes others
+#include <string.h> //includes string functions
+#include <time.h>
+
+char tileMaker(float p){ //makes every tile on the board
+  float random = (float) rand()/RAND_MAX; //random number between 0 and 1
+  char tile; 
+  if (random < p){ //if number less than prob
+    tile = 'X'; //is mine
+  } else {
+    tile = '.'; //else is not mine
+  }
+  return tile;
+}
+
+int findMine(char *board, int row, int column, int m, int n){
+  int count = 0; //counting nearby mines
+
+  //calculating lowest and highest nearby rows
+  int rowLow;
+  int rowHigh;
+  int columnLow;
+  int columnHigh;
+
+  if ((row - 1) < 0) { //finds lowest row
+    rowLow = 0;
+  } else {
+    rowLow = row - 1;
+  }
+
+  if ((row + 1) > (m - 1)) { //finds highest row
+    rowHigh = m - 1;
+  } else {
+    rowHigh = row + 1;
+  }
+
+  if ((column - 1) < 0) { //finds lowest column
+    columnLow = 0; 
+  } else {
+    columnLow = column - 1;
+  }
+
+ if ((column + 1) > (n - 1)) { //finds highest column
+    columnHigh = n - 1;
+  } else {
+    columnHigh = column + 1;
+  }
+
+  for (int i = 0; i < m; i++) { //for every row
+    for (int j = 0; j < n; j++) { //for every column
+      //if current is nearby in row and nearby in column to target position
+      if (((i >= rowLow) && (i <= rowHigh) ) && ((j >= columnLow) && (j <= columnHigh) )) {
+        if (((i * n) + j) == ((row * n) + column)) { //if current is target position
+          continue; //don't count this
+        } else if (board[(i * n) + j] == 'X') { //if current is a mine
+          count++; //iterate up count of mines
+        }
+      }
+    }
+  }
+
+  return count; 
+}
+
+
+int main(int argc, char** argv) {
+  srand((unsigned) time(NULL)); //setting up random time
+  int m;
+  int n;
+  float p;
+  if (argc != 4) { //if not enough arguments
+    //use default values
+    printf("You didn't give me enough arguments? I'm taking over! I like THESE numbers: %d %d %f\n", 3, 5, 0.4);
+    m = 3;
+    n = 5;
+    p = 0.4;
+  } else { //else use command line arguments
+    m = atoi(argv[1]);
+    n = atoi(argv[2]);
+    p = atof(argv[3]);
+  }
+
+  char *board; //pointer to char
+  board = malloc(sizeof(char) * (m * n)); //single call to malloc for char array
+  
+  //making the board
+  for (int i = 0; i < m; i++) { //for every row
+    for (int j = 0; j < n; j++){ //for every column
+      //position is calculated row * n + col
+      board[(i * n) + j] = tileMaker(p); //calling tile making with probability
+    }
+  }
+
+  //printing the board
+  for (int i = 0; i < m; i++) { //for every row
+    for (int j = 0; j < n; j++){ //for every column
+      printf ("%c ", board[(i * n) + j]); //print tiles
+    }
+    printf("\n"); //next row
+  }
+
+  printf("\n"); //spacing before nearby mines
+
+  //printing the nearby mines
+  for (int i = 0; i < m; i++) {  //row
+    for (int j = 0; j < n; j++){ //column
+      if (board[(i * n) + j] == 'X') { //if current is a mine
+        printf ("%c ", 'X'); //print X for mine
+      } else { //if current is not mine
+        printf ("%d ", findMine(board, i, j, m, n)); //print total mines found nearby
+      }
+    }
+    printf("\n"); //next row
+  }
+
+  free(board); //freeing our malloc
   return 0;
 }
--- A03/sorted_restaurants.c
+++ A03/sorted_restaurants.c
@@ -1,3 +1,109 @@
+//Author: Talia Feshbach
+//Date: 9/30/22
+//Name: Sorted Restaurants
+
+#include <stdio.h>
+#include <stdlib.h> //includes others
+#include <string.h> //includes string functions
+
+struct restaurant { //restaurant struct
+  char name[32]; //space for restaurant name
+  int opentime; 
+  int closetime;
+  float stars;
+  struct restaurant* next; //pointer to next restaurant
+};
+
+struct restaurant* insertSorted(struct restaurant* head) { //insert a resturant at the correct place
+  struct restaurant* n = malloc(sizeof(struct restaurant)); //malloc space for the restaurant
+  if (n == NULL) { //if we're out of space
+    printf("ERROR: Out of space!\n"); //scream
+    exit(1);
+  }
+
+  char name[32];
+  int open;
+  int close;
+  float stars;
+  
+  printf("Enter a name: "); 
+  scanf(" %[^\n]%*c", name); //scan in name
+  strcpy(n->name, name);
+
+  printf("Open time: ");
+  scanf(" %d%*c", &open); //scan in opening time
+  n->opentime = open;
+
+  printf("Close time: ");
+  scanf(" %d%*c", &close); //scan in closing time
+  n->closetime = close;
+
+  printf("Stars: ");
+  scanf(" %f%*c", &stars); //scan in stars
+  n->stars = stars;
+
+  if (head == NULL) { //if this is the first node
+    n->next = NULL; //point to null
+    head = n; //this is the head
+  } else if ((head->stars < n->stars)){ //if n stars is bigger than head stars
+      n->next = head; //point to head
+      head = n; //set n as head
+  } else {
+    struct restaurant* temp = head; //temp value is head
+    while (temp != NULL) { //while there is still another node
+      if ((temp->stars >= n->stars) && temp->next == NULL) {
+        //if temp is bigger and there's nothing after it
+        temp->next = n; //head points to n
+        n->next = NULL; //n points to null
+        break;
+      } else if ((temp->stars >= n->stars) && temp->next != NULL) {
+        //if head is bigger than current and something is after it
+          if (temp->next->stars < n->stars) { //if the thing after it is smaller
+            n->next = temp->next;
+            temp->next = n;
+            break;
+          } else { //if it's bigger
+            temp = temp->next; //move temp to it and move on
+          }
+      }
+    }
+  }
+  return head; //return the head
+ }
+
+
+void clear (struct restaurant* head) {
+  struct restaurant* current = head; //current value is head
+  struct restaurant* to_delete = head; //value to delete is head
+  while (current != NULL) { //if the current value exists
+    current = current->next; //current is now next
+    free(to_delete); //delete to delete
+    to_delete = current; //set to delete to current
+  }
+}
+
+
 int main() {
+  int choices;
+  printf("Enter a number of restaurants: ");
+  scanf(" %d", &choices); //scan in number of choices
+
+
+struct restaurant* head = NULL; //null restaurant to be filled later
+
+  for (int i = 0; i < choices; i++) { //for every restaurant
+    head = insertSorted(head); //create a value in list
+  }
+
+  printf("Welcome to Talia Feshbach's Sorted Restarant List.\n");
+  printf("\n"); //print stuff
+
+  struct restaurant* temp = head; //temporary value to iterate through list
+  for (int i = 0; i < choices; i++) { //restaurant table
+    printf("%d) %s    open: %d:00     close: %d:00    stars: %f\n", i, temp->name, temp->opentime, temp->closetime, temp->stars);
+    temp = temp->next; //set temp to next
+  }
+ 
+  clear(head); //clear the list
   return 0;
 }
