a87f7f20da6ad2cf3354500bb56d60c9e3ec9111
alinen <alinen@savvysine.com>
2022-10-01 09:15:03 -0400

    assignment 04

---
+++ A04/ascii_image.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_ppm.h"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("usage: ascii_image <file.ppm>\n");
+    return 0;
+  }
+
+  return 0;
+}
+
---
+++ A04/test_ppm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_ppm.h"
+
+int main() {
+  // load feep-raw and print out the grid of pixels
+  return 0;
+}
+
