efb6d438e159b98149796182ba14b9e11e5dacd3
Talia Feshbach <tfeshbach@brynmawr.edu>
2022-10-28 23:12:45 -0400

    assignment complete

--- A06/decode.c
+++ A06/decode.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: decode <file.ppm>\n");
-    return 0;
+  if (argc != 2) { //if incorrect usage
+    printf("usage: decode <file.ppm>\n"); //scream
+    return 0; //and leave
+  }
+  int w, h; //width and height
+  char* filename = argv[1];
+  struct ppm_pixel* image = read_ppm(filename, &w, &h); //get pixel struct
+  printf("Reading %s with width %d and height %d\n", filename, w, h); //printing info
+  unsigned char* bits = (unsigned char*)image; //cast pixels to unsigned chars
+  char* str = (char*)image; //cast pixels to chars
+  int size = w * h * 3; //size of bits
+  int length = (size)/8; //possible length of message based on amount of available sections of 8
+  printf("Max number of characters in the image: %d\n", length); //print possible length
+
+  unsigned char mask = 0x1; //mask
+  unsigned char dest; //destination of least signigicant bit
+  for (int i = 0; i < length; i++){ //for every char
+    for (int j = 0; j < 8; j++){ //for every bit composing that char
+      dest = mask << (7-j); //destination of this bit is our current place in the char
+      str[i] &= ~dest; //clear that destination in the char
+      str[i] |= ((bits[i*8 + j] & mask) << (7-j)); //after getting the least significant bit, shift it to destination and OR it in 
+    }
   }
 
+  printf("%s\n", str); //print the string 
+  
+  free(image); //free the pixel
+  
   return 0;
 }
 
--- A06/encode.c
+++ A06/encode.c
@@ -5,11 +5,66 @@
 #include "write_ppm.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: encode <file.ppm>\n");
-    return 0;
+  if (argc != 2) { //if incorrect usage
+    printf("usage: encode <file.ppm>\n"); //scream
+    return 0; //and leave
   }
 
+  int w, h; //width and height
+  char* filename = argv[1];
+  struct ppm_pixel* image = read_ppm(filename, &w, &h); //get pixel struct
+  printf("Reading %s with width %d and height %d\n", filename, w, h); //printing info
+  int size = w * h * 3; //size of bits
+  int length = (size)/8; //possible length of message based on amount of available sections of 8
+  printf("Max number of characters in the image: %d\n", length);
+
+  char* phrase; //phrase to encode
+  phrase = malloc(length*sizeof(char)); //malloc its size based on how many chars are available
+  memset(phrase, 0, length*sizeof(char)); //fill it with 0s because valgrind cried when you don't
+  printf("Enter a phrase: ");
+  scanf(" %[^\n]%*c", phrase); //scan in phrase
+
+  char encodedfile[100] = ""; //making the new file name
+  for (int i = 0; i < strlen(filename) - 4; i++){ //reading in all but ".ppm"
+    encodedfile[i] = filename[i]; 
+  }
+  char* code = "-encoded.ppm"; 
+  strcat(encodedfile, code); //appending "-encoded.ppm"
+
+
+
+  unsigned char* bits = (unsigned char*)image; //cast pixels to unsigned chars
+  //unsigned char* str = (unsigned char*)image; //cast pixels to chars
+
+
+  unsigned char mask = 0x1; //mask
+  unsigned char bit; //thing to put in least signigicant bit
+  for (int i = 0; i < length; i++){ //for every char
+    for (int j = 0; j < 8; j++){ //for every bit composing that char
+      bit = (phrase[i] >> (7-j)) & mask; //get relevant bit from string
+      bits[i*8 + j] &= ~mask; //clear the least significant bit in the current char
+      bits[i*8 + j] |= bit; //AND the bit into the least significant place
+    }
+  }
+
+struct ppm_pixel *grid = malloc(sizeof(struct ppm_pixel) * (w * h)); //make new pixel grid
+for (int i = 0; i < size; i++){
+  int index = i % 3; //find value of this position
+  if (index == 0) { //if value is red
+      grid[i/3].red = bits[i]; //set the red value of the correct tile to the char
+    } else if (index == 1) { //if value is green
+      grid[i/3].green = bits[i];
+    } else if (index == 2) { //if value is blue
+      grid[i/3].blue = bits[i]; 
+    }
+}
+
+  printf("Writing file %s\n", encodedfile); 
+  write_ppm(encodedfile, grid, w, h); //write encoded file 
+
+  free(grid); //free the grid we made for encoded image
+  free(image); //free the image opened in the first place
+  free(phrase); //free the phrase they gave us
   return 0;
 }
 
