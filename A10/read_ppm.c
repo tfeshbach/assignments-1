e822f5088742c958a32ab6e5e6e10b3b75047c1a
alinen <alinen@savvysine.com>
2022-11-18 10:03:46 -0500

    post basecode for A09 and A10

---
+++ A10/read_ppm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "read_ppm.h"
+
+struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+
+  return NULL;
+}
+
+struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
+
+  return NULL;
+}
